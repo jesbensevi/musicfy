e9a8ff57febe45f528da9400bf5a78ab9c76b3ed
Jesben Sevillano <jesben.sevillano@cibergestion.com.pe>
2021-02-20 10:10:15 -0500

    user fake

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "./utils/Firebase";
 import "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   firebase.auth().onAuthStateChanged((currentUser) => {
-    console.log(currentUser ? "Estamos logeados" : "No estamos logeados");
+    !currentUser ? setUser(true) : setUser(currentUser);
+    setIsLoading(false);
   });
 
+  if (isLoading) {
+    return null;
+  }
+
+  return !user ? <h1>Usuario no logeado</h1> : <UserLogged />;
+}
+
+function UserLogged() {
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+
   return (
-    <div>
-      <h1> App Electron plus React </h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <h1>Usuario logeado</h1>
+      <button onClick={logout}>Cerrar Sesion</button>
     </div>
   );
 }
